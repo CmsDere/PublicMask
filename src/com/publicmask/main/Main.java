5c025e17358029d12b7e9ee473a08345365aff73
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-04-24 12:18:52 +0900

    큰틀

--- src/com/publicmask/main/Main.java
+++ src/com/publicmask/main/Main.java
@@ -1,7 +1,12 @@
 package com.publicmask.main;
 
+import com.publicmask.view.MainView;
+
 public class Main {
     public static void main(String[] args){
 
+    	MainView mv = new MainView();
+		mv.loginview();
+    	
     }
 }
