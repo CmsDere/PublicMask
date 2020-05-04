dbcc223097fd6d3c986b40c0e9e798a358d5b2f7
CmsDere <airbusboeing3076@gmail.com>
2020-05-04 11:16:56 +0900

    dipose dialog

--- src/com/publicmask/view/MainView.java
+++ src/com/publicmask/view/MainView.java
@@ -622,8 +622,7 @@ public class MainView {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				choice.setVisible(true);
-				
+				choice.setVisible(true);		
 			}
 		});
 		choice = new Dialog(mf, "안내");
@@ -641,6 +640,7 @@ public class MainView {
 				mf.pack();
 				mf.setVisible(true);
 				mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				choice.dispose();
 			}
 		});
 		exit.addActionListener(new ActionListener() {
