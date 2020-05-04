49f2c072e5fb26bef0b0d0dc309256a63c44c978
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-05-04 09:30:04 +0900

    대희 (메인 화면 적용)

--- src/com/publicmask/main/Main.java
+++ src/com/publicmask/main/Main.java
@@ -1,9 +1,13 @@
 package com.publicmask.main;
 
 import com.publicmask.view.MainFrame;
+import com.publicmask.view.MainView;
 
 public class Main {
     public static void main(String[] args){
-    	new MainFrame();
+//    	new MainFrame();
+    	
+    	MainView mv = new MainView();
+		mv.MainView();
     }
 }
