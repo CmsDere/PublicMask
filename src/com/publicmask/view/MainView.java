e6570fd8806377f0ed268734b37c20f897ac9f3d
CmsDere <airbusboeing3076@gmail.com>
2020-05-08 14:38:06 +0900

    커밋

--- src/com/publicmask/view/MainView.java
+++ src/com/publicmask/view/MainView.java
@@ -340,7 +340,7 @@ boolean 		check = mc.adminCheck(text1.getText(), text2.getText());
 				
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					replace(ShowPersonList());
+					replace(BuyerListPanel());
 					mf.setVisible(true);
 					mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					
@@ -374,93 +374,93 @@ boolean 		check = mc.adminCheck(text1.getText(), text2.getText());
 //	}
 			   
 	
-	public JPanel ShowPersonList() {
-//		viewPerson();
-
-		JPanel ShowPersonList = new JPanel();
-		ShowPersonList.setLayout(new BorderLayout());
-
-		Font storef1 = new Font("돋음", Font.BOLD, 30);
-		Font storef2 = new Font("돋음", Font.BOLD, 25);
-		Font storef3 = new Font("돋음", Font.BOLD, 20);
-
-//			      //배열방식
-		String header[] = { "이름", "주민등록번호" };
-
-		DefaultTableModel model = new DefaultTableModel(header, 0);
-		JTable table = new JTable(model);
-
-		// 행 받아오기
-//		Object[] personName = mc.getpersonList().toArray();
-//		Object[] personNum = mc.getpersonList().toArray();
+//	public JPanel ShowPersonList() {
+////		viewPerson();
 //
-//		for (int i = 0; i < mc.getpersonList().size(); i++) {
-//			personName[i] = mc.getpersonList().get(i).getUserName();
-//			personNum[i] = mc.getpersonList().get(i).getUserNumber();
+//		JPanel ShowPersonList = new JPanel();
+//		ShowPersonList.setLayout(new BorderLayout());
+//
+//		Font storef1 = new Font("돋음", Font.BOLD, 30);
+//		Font storef2 = new Font("돋음", Font.BOLD, 25);
+//		Font storef3 = new Font("돋음", Font.BOLD, 20);
+//
+////			      //배열방식
+//		String header[] = { "이름", "주민등록번호" };
+//
+//		DefaultTableModel model = new DefaultTableModel(header, 0);
+//		JTable table = new JTable(model);
+//
+//		// 행 받아오기
+////		Object[] personName = mc.getpersonList().toArray();
+////		Object[] personNum = mc.getpersonList().toArray();
+////
+////		for (int i = 0; i < mc.getpersonList().size(); i++) {
+////			personName[i] = mc.getpersonList().get(i).getUserName();
+////			personNum[i] = mc.getpersonList().get(i).getUserNumber();
+////
+////		}
+////
+////		for (int i = 0; i < mc.getpersonList().size(); i++) {
+////			String[] contents = new String[2];
+////
+////			contents[0] = (String) personName[i];
+////			contents[1] = (String) personNum[i];
+////
+////			model.addRow(contents);
+////
+////			contents[0] = null;
+////			contents[1] = null;
+////
+////		}
+//
+//		table.getTableHeader().setBackground(Color.white);
+//		table.setPreferredScrollableViewportSize(new Dimension(525, 200)); // 테이블 크기
+//
+//		// 제목 패널
+//		JPanel Panel1 = new JPanel();
+//
+//		Panel1.setPreferredSize(new Dimension(525, 50)); // 패널 사이즈 설정으로 그 전화면과 사이즈 통일
+//		Panel1.setBackground(new Color(82, 204, 250));
+//
+//		JLabel titleLabel = new JLabel("구매자 리스트");
+//		titleLabel.setBackground(Color.white);
+//		titleLabel.setForeground(Color.white);
+//		titleLabel.setHorizontalAlignment(JLabel.CENTER);
+//		titleLabel.setFont(storef1);
+//
+//		Panel1.add(titleLabel);
+//
+//		JScrollPane scrollpane = new JScrollPane(table);
+//		scrollpane.setBackground(Color.WHITE);
+//
+//		// 가운데 정렬위한 객체 생성
+//		DefaultTableCellHeaderRenderer celAlignCenter = new DefaultTableCellHeaderRenderer();
+//		celAlignCenter.setHorizontalAlignment(JLabel.CENTER);
+//
+//		TableColumnModel tcmSchedule = table.getColumnModel();
+//
+//		// 반복문을 이용하여 테이블을 가운데 정렬로 지정
+//
+//		for (int i = 0; i < tcmSchedule.getColumnCount(); i++) {
+//
+//			tcmSchedule.getColumn(i).setCellRenderer(celAlignCenter);
 //
 //		}
 //
-//		for (int i = 0; i < mc.getpersonList().size(); i++) {
-//			String[] contents = new String[2];
+//		JPanel btnPanel = new JPanel();
 //
-//			contents[0] = (String) personName[i];
-//			contents[1] = (String) personNum[i];
+//		JButton btn = new JButton("이전으로");
 //
-//			model.addRow(contents);
+//		btnPanel.add(btn);
+//		btnPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
 //
-//			contents[0] = null;
-//			contents[1] = null;
+//		ShowPersonList.add(Panel1, "North");
+//		ShowPersonList.add(scrollpane, BorderLayout.CENTER);
+//		ShowPersonList.add(btnPanel, "South");
 //
-//		}
-
-		table.getTableHeader().setBackground(Color.white);
-		table.setPreferredScrollableViewportSize(new Dimension(525, 200)); // 테이블 크기
-
-		// 제목 패널
-		JPanel Panel1 = new JPanel();
-
-		Panel1.setPreferredSize(new Dimension(525, 50)); // 패널 사이즈 설정으로 그 전화면과 사이즈 통일
-		Panel1.setBackground(new Color(82, 204, 250));
-
-		JLabel titleLabel = new JLabel("구매자 리스트");
-		titleLabel.setBackground(Color.white);
-		titleLabel.setForeground(Color.white);
-		titleLabel.setHorizontalAlignment(JLabel.CENTER);
-		titleLabel.setFont(storef1);
-
-		Panel1.add(titleLabel);
-
-		JScrollPane scrollpane = new JScrollPane(table);
-		scrollpane.setBackground(Color.WHITE);
-
-		// 가운데 정렬위한 객체 생성
-		DefaultTableCellHeaderRenderer celAlignCenter = new DefaultTableCellHeaderRenderer();
-		celAlignCenter.setHorizontalAlignment(JLabel.CENTER);
-
-		TableColumnModel tcmSchedule = table.getColumnModel();
-
-		// 반복문을 이용하여 테이블을 가운데 정렬로 지정
-
-		for (int i = 0; i < tcmSchedule.getColumnCount(); i++) {
-
-			tcmSchedule.getColumn(i).setCellRenderer(celAlignCenter);
-
-		}
-
-		JPanel btnPanel = new JPanel();
-
-		JButton btn = new JButton("이전으로");
-
-		btnPanel.add(btn);
-		btnPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-
-		ShowPersonList.add(Panel1, "North");
-		ShowPersonList.add(scrollpane, BorderLayout.CENTER);
-		ShowPersonList.add(btnPanel, "South");
-
-		return ShowPersonList;
-
-	}
+//		return ShowPersonList;
+//
+//	}
 		
 		public JPanel adminStorelistViewPanel() {
 			
